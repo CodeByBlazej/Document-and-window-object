d324965ba458d989d001d6d5d7affce823f29d96
CodeByBlazej <codebyblazej@gmail.com>
2023-12-01 19:14:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ const button = document.querySelector('button');
 section.className = 'red-bg';
 
 button.addEventListener('click', () => {
-  if (section.className === 'red-bg visible') {
-    section.className = 'red-bg invisible';
-  } else {
-    section.className = 'red-bg visible';
-  }
+  // if (section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visible';
+  // }
+
+  // section.classList.toggle('visible');
+  section.classList.toggle('invisible');
 });
